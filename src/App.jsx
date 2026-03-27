22e648a1450acba6c0f7d94f0e769fca4496b158
Hour <menghour1597@gmail.com>
2026-03-27 23:06:35 +0700

    Fix About page and update imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar'
-import { Body } from './components/body'
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './page/Home'
 import { Contact } from './page/Contact'
-import { About } from './page/about'
 import { Service } from './page/Service'
 
 function App() {
@@ -16,7 +14,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+
         <Route path="/services" element={<Service />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
