180d64822f705225d0df73ca1eede38fd1523da7
Hour <menghour1597@gmail.com>
2026-03-27 23:12:56 +0700

    Fix About page and imports

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from './page/Home'
 import { Contact } from './page/Contact'
 import { Service } from './page/Service'
+import { About } from './page/About'
 
 function App() {
   const fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
@@ -14,7 +15,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-
+        <Route path='/about' element={<About/>}></Route>
         <Route path="/services" element={<Service />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
